1daa70cb9c2438f91fd2f4cbe08a6a96f579f29e
高建栋 <gaojiandong@wilddog.com>
2015-09-25 14:58:09 +0800

    create weather demo

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by IMacLi on 15/7/28.
+//  Copyright (c) 2015年 IMacLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
